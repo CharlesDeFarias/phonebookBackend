35f86950c56a1e31f5f7317f2ffc64a3c95d3aaa
Charles De Farias <cddefari@gmail.com>
2025-03-14 04:53:19 -0400

    got it working at baseline, now have to add functionality of saving number and fetching all of them through command line

---
+++ mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://a:${password}@cluster0.nvtll.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: 'test1.0',
+  number: 'test2.0',
+})
+
+person.save().then(result => {
+  console.log('Person saved!')
+  mongoose.connection.close()
+})
